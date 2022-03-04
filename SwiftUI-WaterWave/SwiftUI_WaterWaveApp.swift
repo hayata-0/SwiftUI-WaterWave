19aaedc6407f30d6d93b41cc39e7d65f6016b3b9
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-03-04 23:51:12 +0900

    Initial Commit

---
+++ SwiftUI-WaterWave/SwiftUI_WaterWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WaterWaveApp.swift
+//  SwiftUI-WaterWave
+//
+//  Created by 大野颯太 on 2022/03/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WaterWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
