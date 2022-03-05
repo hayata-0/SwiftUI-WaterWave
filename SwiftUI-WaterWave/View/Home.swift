a9d6b4f223b03d06e00dbf6c4c2979f0cc85e2e3
hayata-0 <soccerkozo8814@docomo.ne.jp>
2022-03-05 10:50:23 +0900

    Waveを実装

--- SwiftUI-WaterWave/View/Home.swift
+++ SwiftUI-WaterWave/View/Home.swift
@@ -35,6 +35,9 @@ struct Home: View {
                         .aspectRatio(contentMode: .fit)
                         .foregroundColor(.white)
                         .scaleEffect(x: 1.1, y: 1)
+                    
+                    //wave form shape
+                    WaterWave(progress: 0.5, waveHeight: 0.1, offset: size.width)
                 }
                 .frame(width: size.width, height: size.height, alignment: .center)
             }
@@ -52,3 +55,36 @@ struct Home_Previews: PreviewProvider {
     }
 }
 
+struct WaterWave:Shape {
+    var progress: CGFloat
+    
+    var waveHeight: CGFloat
+    
+    var offset: CGFloat
+    
+    var animatableData: CGFloat{
+        get{offset}
+        set{offset = newValue}
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        return Path {path in
+            path.move(to: .zero)
+            let progressHeight:CGFloat = (1 - progress) * rect.height
+            let height = waveHeight * rect.height
+            
+            for value in stride(from: 0, to: rect.width, by: 2) {
+                let x:CGFloat = value
+                let sine:CGFloat = sin(Angle(degrees: value + offset).radians)
+                let y:CGFloat = progressHeight + (height * sine)
+                
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+            
+            //Bottom portion
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height))
+            path.addLine(to: CGPoint(x: 0, y: rect.height))
+            
+        }
+    }
+}
